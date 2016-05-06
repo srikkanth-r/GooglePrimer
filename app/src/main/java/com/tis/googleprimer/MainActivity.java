f94bcc1e03c82ed1b35b8a5e5389ce8c9636f222
srikkanth <srikkanth.r@gmail.com>
2016-05-06 20:58:57 +0530

    Added card layout

--- app/src/main/java/com/tis/googleprimer/MainActivity.java
+++ app/src/main/java/com/tis/googleprimer/MainActivity.java
@@ -2,12 +2,19 @@ package com.tis.googleprimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView mCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCard = (CardView) findViewById(R.id.card);
+
     }
 }
